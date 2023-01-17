061de3f6688650e0403d31f026d56007364404ff
dhanush.ramesh <dhanush.ramesh@zohocorp.com>
2023-01-17 22:37:24 +0530

    Add router, buefy and home page

---
+++ mafia-game/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-container">
+    <img :src="require('@/assets/mafia.png')" alt="" class="brand-img" />
+    <b-button class="start-btn" rounded type="is-danger" tag="router-link" to="/new">Play</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+};
+</script>
+
+<style>
+@import "~@/css/homePageStyle.css";
+</style>
---
+++ mafia-game/src/router/router.js
@@ -0,0 +1,30 @@
+// ignorei18n_start
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/components/Home.vue"),
+    meta: {
+      hideNavbar: true
+    }
+  },
+  {
+    path: "/new",
+    name: "NewGame",
+    component: () => import("@/components/NewGame.vue")
+  }
+];
+
+const router = new VueRouter({
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
+
+// ignorei18n_end
